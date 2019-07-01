f4b91e201907ece5c0a295d14526eb4b5ceefd95
mfm1 <48580544+mfm1@users.noreply.github.com>
2019-07-01 19:41:14 +0300

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,13 +1,13 @@
 """
     This program works based on the Collatz Sequence
-    It devides the number given by the user un it is no 
+    It devides the number given by the user untill it is no 
     more divisible ( i.e the number is devided untill it is equal to 1)
 """
 
-def collatz(num):
-    #Check to see if num is an even number
-    if num % 2 == 0:
-        n = num // 2
+def collatz(number):
+    #Check to see if number is an even number
+    if number % 2 == 0:
+        n = number // 2
         if n != 1:
             print(n)
             return collatz(n)
@@ -15,10 +15,10 @@ def collatz(num):
             print(n)
 
     #Check to see if the number is an odd number
-    elif num % 2 != 0:
+    elif number % 2 != 0:
         """n converts the odd number to even number 
             and pass it back to collatz"""
-        n = 3 * num + 1
+        n = 3 * number + 1
         print(n)
         return collatz(n)
     
